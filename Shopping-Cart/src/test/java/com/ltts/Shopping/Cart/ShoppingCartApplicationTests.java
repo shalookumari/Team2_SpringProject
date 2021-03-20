72b93cf529d215b3f5a4ad5895cff08dd77370b2
shalookumari <78845430+shalookumari@users.noreply.github.com>
2021-03-20 13:59:25 +0530

    project

---
+++ Shopping-Cart/src/test/java/com/ltts/Shopping/Cart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.Shopping.Cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
